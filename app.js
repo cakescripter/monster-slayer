f49a7bb43d8d4d29c14d7ee947bb7597e7b4ba77
cakescripter <cakescripter@gmail.com>
2023-07-25 14:47:00 +0200

    attack

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
+function getAttackValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+}
 const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            pminAP: 5,
+            pmaxAP: 12,
 
+            monsterHealth: 100,
+            mminAP: 8,
+            mmaxAP: 15,
+        }
+    },
+    computed: {
+        healthBarM() {
+            return { width: this.monsterHealth + '%' }
+        },
+        healthBarP() {
+            return { width: this.playerHealth + '%' }
+        }
+    },
+    methods: {
+        attackMonster() {
+            const attackValue = getAttackValue(this.pminAP, this.pmaxAP);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getAttackValue(this.mminAP, this.mmaxAP);
+            this.playerHealth -= attackValue;
+        }
+    }
 })
 app.mount('#game');
